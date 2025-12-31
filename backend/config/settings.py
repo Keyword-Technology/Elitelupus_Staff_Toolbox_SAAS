537c47a2a489aee265a9f3c7cd2d9f32c3e440d9
Connor <hessconnor41@gmail.com>
2025-12-31 05:57:48 +0200

    feat: Disable automatic slash appending for REST API

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -65,6 +65,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
+# Disable automatic slash appending for REST API
+APPEND_SLASH = False
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
